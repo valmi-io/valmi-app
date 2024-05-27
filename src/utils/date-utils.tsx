7151215d12745789f7256dde1f40b9e7feb5f438
Tharun Nookala <160469490+tharun-n@users.noreply.github.com>
2024-05-27 13:35:37 +0530

    Feat.explores (#66)
    * feat: update explores UI

--- src/utils/date-utils.tsx
+++ src/utils/date-utils.tsx
@@ -3,3 +3,24 @@ import moment from 'moment';
 export const getLastNthDate = (n: number) => {
   return moment().subtract(n, 'days').format('DD-MM-YYYY');
 };
+
+export const getTimeAt = (date: Date) => {
+  return moment(date).format('YYYY-MM-DD');
+};
+
+export const getTimeAgo = (date: Date | '') => {
+  const now = moment();
+  if (date === '') {
+    return '--';
+  } else {
+    const diff = now.diff(date, 'days');
+
+    if (diff === 0) {
+      return 'Today';
+    } else if (diff === 1) {
+      return '1 day ago';
+    } else {
+      return diff + ' days ago';
+    }
+  }
+};
