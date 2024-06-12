e753047c961b1030862c26a91718d08795eac1d2
Nagendra <sankojunagendra23@gmail.com>
2024-06-12 12:57:06 +0530

    fix: remove button in the header

--- pages/spaces/[wid]/data-flows/index.tsx
+++ pages/spaces/[wid]/data-flows/index.tsx
@@ -1,25 +1,14 @@
-/*
- * Connections page
- * This component represents a page for displaying connections and creating / editing connection.
- */
-
-import { ReactElement, useEffect } from 'react';
-
-import { useRouter } from 'next/router';
-
-import { useDispatch } from 'react-redux';
+import { ReactElement } from 'react';
 
 import { NextPageWithLayout } from '@/pages_app';
 
 import PageLayout from '@layouts/PageLayout';
 import SidebarLayout from '@layouts/SidebarLayout';
 
-import { AppDispatch } from '@store/store';
 import ContentLayout from '@/layouts/ContentLayout';
 import ListEmptyComponent from '@/components/ListEmptyComponent';
 import { useFetch } from '@/hooks/useFetch';
 import { useFetchSyncsQuery } from '@/store/api/apiSlice';
-import { clearConnectionFlowState } from '@/store/reducers/connectionDataFlow';
 import { useWorkspaceId } from '@/hooks/useWorkspaceId';
 import DataFlows from '@/content/DataFlows';
 
@@ -35,9 +24,6 @@ const PageContent = ({ data }: { data: any }) => {
 };
 
 const DataFlowsPage: NextPageWithLayout = () => {
-  const router = useRouter();
-  const dispatch = useDispatch<AppDispatch>();
-
   const { workspaceId = '' } = useWorkspaceId();
 
   const {
@@ -49,22 +35,8 @@ const DataFlowsPage: NextPageWithLayout = () => {
     query: useFetchSyncsQuery({ workspaceId }, { refetchOnMountOrArgChange: true, skip: !workspaceId })
   });
 
-  const handleCreateConnectionOnClick = () => {
-    router.push(`/spaces/${workspaceId}/catalog`);
-  };
-
-  useEffect(() => {
-    // initialising sync flow state
-    dispatch(clearConnectionFlowState());
-  }, []);
-
   return (
-    <PageLayout
-      pageHeadTitle="Data Flows"
-      title="DATA FLOWS"
-      buttonTitle="Connection"
-      handleButtonOnClick={handleCreateConnectionOnClick}
-    >
+    <PageLayout pageHeadTitle="Data Flows" title="DATA FLOWS" displayButton={false}>
       <ContentLayout
         key={`connectionsPage`}
         error={error}
